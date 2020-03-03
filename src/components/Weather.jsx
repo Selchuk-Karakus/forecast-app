44cc9ec5cc11495d22d0dec667ef4121fd4250d6
Selchuk Karakus <selchuck.karakus@gmail.com>
2020-03-03 20:21:49 +0000

    create weather component

---
+++ src/components/Weather.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Weather = () => {
+    return(
+        <div className="container">
+            <div className="cards">
+                <h1>London</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Weather;
